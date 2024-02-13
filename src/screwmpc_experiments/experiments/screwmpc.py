2356042f73d01ac9d9b08ba2b6d742df350144e7
JeanElsner <jean.elsner@googlemail.com>
2024-02-13 21:11:56 +0100

    chore: add random poses, motion to experiment

--- src/screwmpc_experiments/experiments/screwmpc.py
+++ src/screwmpc_experiments/experiments/screwmpc.py
@@ -1,10 +1,58 @@
 from __future__ import annotations
 
+import contextlib
+import logging
+
 import dm_env
+import dqrobotics
 import numpy as np
+import panda_py
+import spatialmath
 from dm_env import specs
+from dm_robotics.geometry import pose_distribution
 from dqrobotics import robots
 from screwmpcpy import pandamg, screwmpc
+
+logger = logging.getLogger("screwmpc")
+
+
+def pose_to_dq(pose: tuple[np.ndarray, np.ndarray]) -> dqrobotics.DQ:
+    """Computes a unit dual quaternion from a pose.
+
+    Args:
+      pose: The pose given as a tuple consisting of a 3-vector
+        and a unit quaternion.
+    """
+    se3 = (
+        spatialmath.SE3(*pose[0])
+        * spatialmath.UnitQuaternion(pose[1][0], pose[1][1:]).SE3()
+    )
+    return dqrobotics.DQ(spatialmath.UnitDualQuaternion(se3).vec)  # pylint: disable=no-member
+
+
+def generate_random_poses(
+    n: int,
+    min_pose_bounds: np.ndarray,
+    max_pose_bounds: np.ndarray,
+    random_state: np.random.RandomState,  # pylint: disable=no-member
+) -> list[tuple[np.ndarray, np.ndarray]]:
+    """
+    Generate random poses within the given bounds.
+
+    Compute random poses within the given bounds. The poses
+    are checked using inverse kinematics.
+    """
+    gripper_pose_dist = pose_distribution.UniformPoseDistribution(
+        min_pose_bounds=min_pose_bounds,
+        max_pose_bounds=max_pose_bounds,
+    )
+    poses: list[np.ndarray] = []
+    while len(poses) < n:
+        pose = gripper_pose_dist.sample_pose(random_state)
+        q = panda_py.ik(pose[0], pose[1])
+        if not np.any(np.isnan(q)):
+            poses.append(pose)
+    return poses
 
 
 class ScrewMPCAgent:
@@ -12,7 +60,62 @@ class ScrewMPCAgent:
 
     def __init__(self, spec: specs.BoundedArray) -> None:
         self._spec = spec
+        self._waypoints: list[tuple[np.ndarray, np.ndarray]] = []
+        self._goal: dqrobotics.DQ | None = None
+        self.init_screwmpc()
 
+    def add_waypoint(self, waypoint: tuple[np.ndarray, np.ndarray]) -> None:
+        """Adds a waypoint to the buffer.
+
+        Waypoints will be moved through consecutively using the
+        screwmpc motion generator.
+
+        Args:
+          waypoint: Waypoint to add to buffer representing a goal pose
+            given as a tuple containing a 3-vector and a quaternion.
+        """
+        self._waypoints.append(waypoint)
+
+    def step(self, timestep: dm_env.TimeStep) -> np.ndarray:
+        """Computes an action given a timestep observation.
+
+        The action is computed with the screwmpc motion generator to reach
+        the current goal. Once the goal has been reached, the next waypoint
+        added with :py:func:`add_waypoint` becomes the goal.
+        """
+        action = np.zeros(shape=self._spec.shape, dtype=self._spec.dtype)
+        if self._goal is not None:
+            action[:7] = self.motion_generator.step(
+                timestep.observation["panda_joint_pos"], self._goal
+            )
+        if self.at_goal(timestep):
+            logger.info("Goal reached.")
+            self._goal = None
+        if self._goal is None:
+            with contextlib.suppress(IndexError):
+                self._goal = pose_to_dq(self._waypoints.pop(0))
+                logger.info("Tracking new goal.")
+
+        return action
+
+    def at_goal(self, timestep: dm_env.TimeStep) -> bool:
+        """Checks whether the agent has reached the current goal."""
+        if self._goal is None:
+            return False
+        return bool(
+            np.linalg.norm(
+                (
+                    self.kinematics.fkm(timestep.observation["panda_joint_pos"])
+                    - self._goal
+                )
+                .norm()
+                .vec8()
+            )
+            < 0.05
+        )
+
+    def init_screwmpc(self) -> None:
+        """Initializes the screwmpc motion generator."""
         n_p = 50  # prediction horizon, can be tuned;
         n_c = 10  # control horizon, can be tuned
         R = 10e-3  # weight matirix
@@ -35,10 +138,3 @@ class ScrewMPCAgent:
             n_p, n_c, Q, R, vel_bound, acc_bound, jerk_bound
         )
         self.kinematics = robots.FrankaEmikaPandaRobot.kinematics()  # pylint: disable=no-member
-
-    def step(self, timestep: dm_env.TimeStep) -> np.ndarray:
-        """Computes an action given a timestep observation.
-        The action is computed with the screwmpc motion generator.
-        """
-        del timestep
-        return np.zeros(shape=self._spec.shape, dtype=self._spec.dtype)
--- src/screwmpc_experiments/launchers/random.py
+++ src/screwmpc_experiments/launchers/random.py
@@ -7,13 +7,13 @@ from __future__ import annotations
 import argparse
 import logging
 
-from dm_robotics.panda import environment, run_loop, utils
+import numpy as np
+from dm_robotics.panda import arm_constants, environment, run_loop, utils
 from dm_robotics.panda import parameters as params
 
 from ..experiments import screwmpc
 
-logging.basicConfig(level=logging.INFO)
-logger = logging.Logger("screwmpc-random")
+logging.basicConfig(level=logging.INFO, force=True)
 
 
 def main() -> None:
@@ -21,6 +21,8 @@ def main() -> None:
     Main function of this experiment.
     Run from the terminal by executing `screwmpc-random`.
     """
+    logging.basicConfig(level=logging.INFO, force=True)
+
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--robot-ip", default=None, type=str, help="Robot IP or hostname for HIL"
@@ -30,14 +32,29 @@ def main() -> None:
     )
     args = parser.parse_args()
 
-    robot_params = params.RobotParams(robot_ip=args.robot_ip)
-    panda_env = environment.PandaEnvironment(robot_params)
+    robot_params = params.RobotParams(
+        robot_ip=args.robot_ip, actuation=arm_constants.Actuation.JOINT_VELOCITY
+    )
+    panda_env = environment.PandaEnvironment(robot_params, control_timestep=0.01)
 
     with panda_env.build_task_environment() as env:
-        # Print the full action, observation and reward specification.
-        # utils.full_spec(env)
-        # Initialize the agent.
+        # Use a fixed seed for reproducibility
+        rng = np.random.RandomState(seed=1)  # pylint: disable=no-member
+        # Generate 10 random poses within these bounds
+        # given as x, y, z (linear) and X, Y, Z (angular, euler angles)
+        min_pose_bounds = np.array(
+            [0.5, -0.3, 0.7, 0.75 * np.pi, -0.25 * np.pi, -0.25 * np.pi]
+        )
+        max_pose_bounds = np.array(
+            [0.1, 0.3, 0.1, 1.25 * np.pi, 0.25 * np.pi / 2, 0.25 * np.pi]
+        )
+        poses = screwmpc.generate_random_poses(
+            10, min_pose_bounds, max_pose_bounds, rng
+        )
         agent = screwmpc.ScrewMPCAgent(env.action_spec())
+        for p in poses:
+            agent.add_waypoint(p)
+
         # Run the environment and agent either in headless mode or inside the GUI.
         if not args.no_gui:
             app = utils.ApplicationWithPlot()
